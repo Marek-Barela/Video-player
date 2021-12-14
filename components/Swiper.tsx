5d8337300c54ac09d4b1ca798f4b45dc5311a35f
Marek Bareła <marek@alergeek.ventures>
2021-12-14 21:46:02 +0100

    feat: add typescript for data reequest and update swiper styling

--- components/Swiper.tsx
+++ components/Swiper.tsx
@@ -1,15 +1,60 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Box, Text } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Box, Flex,Spinner, Text } from '@chakra-ui/react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+interface Products {
+  Id: number;
+}
+
+interface Image {
+  Height: number;
+  Id: number;
+  ImageTypeCode: string;
+  MediaId: number;
+  PlatformCode: string;
+  Url: string;
+  Width: number;
+}
+
+interface EntityItem {
+  AvailableFrom: string;
+  Description: string;
+  Guid: string;
+  Id: number;
+  Images: Image[];
+  IsTrialContentAvailable: boolean;
+  MediaAgeRestrictionImageUrl: string;
+  MediaAgeRestrictionValueMin: number;
+  MediaTypeCode: string;
+  MediaTypeDisplayName: string;
+  OrderInParent: number;
+  ParentMediaId: number;
+  ParentMediaTitle: string;
+  ParentOrderInParent: number;
+  Products: Products[];
+  Title: string;
+}
+
+type MediaData = {
+  CacheDataValidTo: string;
+  Entities: EntityItem[];
+  PageNumber: number;
+  PageSize: number;
+  SourceType: string;
+  TotalCount: number;
+};
 
 interface SwiperBoxProps {
   id: number;
 }
 
 const SwiperBox = ({ id }: SwiperBoxProps) => {
-  const [videoData, setVideoData] = useState({});
+  const [videoData, setVideoData] = useState<MediaData>();
 
   useEffect(() => {
     const token = localStorage.getItem('Token') as string;
@@ -39,21 +84,32 @@ const SwiperBox = ({ id }: SwiperBoxProps) => {
 
   return (
     <Box margin={'30px'}>
-      <Swiper spaceBetween={30} slidesPerView={3}>
-        {videoData?.Entities?.map((item) => (
-          <SwiperSlide key={item.Guid}>
-            {item?.Images[0]?.Url ? (
-              <Box
-                bgImage={item.Images[0].Url}
-                minH={'300px'}
-                bgSize={'cover'}
-                borderRadius={'10px'}
-              />
-            ) : (
-              <Text>No Image</Text>
-            )}
-          </SwiperSlide>
-        ))}
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+      >
+        {videoData ? (
+          videoData?.Entities?.map((item) => (
+            <SwiperSlide key={item.Guid}>
+              {item?.Images[0]?.Url ? (
+                <Box
+                  bgImage={item.Images[0].Url}
+                  minH={'300px'}
+                  bgSize={'cover'}
+                  borderRadius={'10px'}
+                />
+              ) : (
+                <Text>No Image</Text>
+              )}
+            </SwiperSlide>
+          ))
+        ) : (
+          <Flex justifyContent={'center'} alignItems={'center'} minH={'200px'}>
+            <Spinner />
+          </Flex>
+        )}
       </Swiper>
     </Box>
   );
